ad67fd431136a72e3f1eabb1b9fcf2f49ae081a2
Manmeet Tarun <meetutarun16@gmail.com>
2018-02-12 03:14:32 +0530

    Dialogs working

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var builder=require('botbuilder');
 var restify=require('restify');
+var quiz=require('./api');
 
 var server=restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function(){
@@ -16,3 +17,37 @@ server.post('/api/messages',connector.listen())
 var bot=new builder.UniversalBot(connector,function(session){
     session.send("You said %s",session.message.text);
 });
+
+var username="manmeet17";
+var index=0;
+
+(function(){
+    if(username)
+        quiz.getStats(username);
+})();
+
+bot.dialog('/',
+    function(session){
+        session.send("Quizlet Chatbot");
+        session.beginDialog('/user');
+});
+
+bot.dialog('/user', new builder.IntentDialog()
+    .matches(/^yes/i,[
+        function(session){
+            if(username)
+                session.beginDialog('/subject');
+            else{
+                builder.Prompts.text(session,"What is your quizlet username?")
+            }
+        },
+        function(session,results){
+            quiz.getStats(results.response);
+            session.beginDialog('/subject');
+        }])
+    .matches(/^no/i,function(session){
+        session.send("Ok bye!");
+        session.endConversation;
+}));
+
+bot.dialog('/subject',)
